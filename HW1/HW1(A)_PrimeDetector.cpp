bc5595e9e579617fbf39d29f757c486783fc10cc
Yan-Cheng-Hsue <dragonbily13579@gmail.com>
2020-03-04 13:45:28 +0800

    initialized the HW1(A))_PrimeDetector.cpp

---
+++ HW1/HW1(A)_PrimeDetector.cpp
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<iostream>
+#include<algorithm>
+
+
+//PROBLEM:
+/*
+Write a program to detect an integer n (n > 2) if n is a prime.
+
+if (n is a prime)
+    return true;
+else 
+    return false;
+
+*/
+
+
+class Solution
+{
+    public:
+        bool PrimeDetection( int n );
+};
+
+bool Solution::PrimeDetection( int n );
+{
+
+}
+
+
+void main(void)
+{
+
+
+    system( "pause" );
+    return;
+}
+
+
