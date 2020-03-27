51e12a68dc3e1354d19af84d108aeaefbcb2fb8f
Yan-Cheng-Hsue <dragonbily13579@gmail.com>
2020-03-27 10:33:19 +0800

    HW3 finished.

--- HW3/HanmiltonLoop.cpp
+++ HW3/HanmiltonLoop.cpp
@@ -16,6 +16,7 @@ class Solution
         int size;
         bool **adj;
         int *Solution;
+        bool *visit;
 };
 
 
@@ -52,6 +53,7 @@ void Solution::Input()
 void Solution::Preprocessing()
 {
     //Initialization of size of adjancency matrix and Solution matrix
+
     int max = 0;
     for(int i=0;i<input.size();i++)
     {
@@ -62,6 +64,7 @@ void Solution::Preprocessing()
 
 
     //Initialization of adjancency matrix
+
     this -> adj = new bool*[size];
     for(int i=0;i<size;i++)
         adj[i] = new bool[size];
@@ -72,28 +75,91 @@ void Solution::Preprocessing()
     }
     
     for(int i=0;i<input.size();i=i+2)
+    {
         adj[input[i]][input[i+1]] = true;
+        adj[input[i+1]][input[i]] = true;
+    }
 
+    //Initialization of Solution matrix
 
-    //Initialization of Solution
     this -> Solution = new int[size];
     for(int i=0;i<size;i++)
         Solution[i] = -1;
     
+    //Initialization of visit matrix
 
+    this -> visit = new bool[size];
+    for(int i=0;i<size;i++)
+        visit[i] = false;
+    
 
     return;
 }
 
+
+bool Solution::HamiltonLoop()
+{
+    int k = 1;
+    //Start from Vertex 0 to visit
+    //Actually, starting point makes no difference to whether there is a H loop.
+    this -> Solution[0] = 0;
+    this -> visit[Solution[0]] = true;
+
+    while( k > 0 )
+    {
+        Solution[k] = Solution[k] + 1;
+        while( k < size )
+        {
+            if( ( !visit[Solution[k]] ) && ( adj[Solution[k-1]][Solution[k]] ) )
+                break;
+            else 
+                Solution[k] = Solution[k] + 1;
+        }
+
+        if( ( Solution[k] < size ) && ( k == size - 1 ) && (adj[Solution[k]][Solution[0]]) )
+            break;
+        else if( ( k < size ) && ( Solution[k] < size ) )
+        {
+            visit[Solution[k]] = true;
+            k = k + 1;
+        }
+        else
+        {
+            Solution[k] = -1;
+            k = k - 1;
+            visit[Solution[k]] = false;
+        }
+    }
+
+    if( k == ( size - 1 ) )
+        return true;
+    else 
+        return false;
+}
 
 
 
 
 void test()
 {
-    Solution S;
-    S.Input();
-    S.Preprocessing();
+    vector<string> Output;
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        Solution S;
+        S.Input();
+        S.Preprocessing();
+        if( S.HamiltonLoop() )
+            Output.push_back( "True" );
+        else
+            Output.push_back( "False" );
+        
+    }
+
+    for(int i=0;i<Output.size();i++)
+        cout<<Output[i]<<endl;
+    
 
     return;
 }
@@ -102,4 +168,4 @@ int main(void)
 {
     test();
     return 0;
-}
+} 
